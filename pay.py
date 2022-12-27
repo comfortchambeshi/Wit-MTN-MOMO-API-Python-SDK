137af87dc74365420e07e477cb83cadb4fe825e1
justinekatebe69 <67024464+justinekatebe69@users.noreply.github.com>
2022-12-27 18:52:13 +0200

    Update pay.py
    I've added the ability to test disbursements in test mode for the sandbox. Changes made/added:
    
    1. Create API User for disbursements
    2. Create API Key for disbursements

--- pay.py
+++ pay.py
@@ -5,187 +5,247 @@ import uuid
 from basicauth import encode
 
 
-
-
-
 class PayClass():
-    #Keys
-    #Collections Subscription Key:
-    collections_subkey = "{{Collections_subscription_key}}"
-    #Disbursement subscription key
-    disbursment_subkey = ""
-    #Production collections basic authorisation key(Leave it blank if in sandbox mode)
+    # Keys
+    # Collections Subscription Key:
+    collections_subkey = "d350d3a623d1447088942fb03b1b60b2"
+    # Disbursement subscription key
+    disbursements_subkey = "73b64d8af5ec40129d6ca982fb01cf85"
+    # Production collections basic authorisation key(Leave it blank if in sandbox mode)
     basic_authorisation_collections = ""
-    #Production disbursement basic authorisation key(Leave it blank if in sandbox mode)
-    basic_authorisation_disbursment = ""
-    
-    #API user and Key(Note: Only use this when in production mode)
+    # Production disbursement basic authorisation key(Leave it blank if in sandbox mode)
+    basic_authorisation_disbursments = ""
+
+    # API user and Key(Note: Only use this when in production mode)
     collections_apiuser = ""
     api_key_collections = ""
 
-    #Application mode
+    # API user and Key(Note: Only use this when in production mode)
+    disbursements_apiuser = ""
+    api_key_disbursements = ""
+
+    # Application mode
     environment_mode = "sandbox"
     accurl = "https://proxy.momoapi.mtn.com"
     if environment_mode == "sandbox":
         accurl = "https://sandbox.momodeveloper.mtn.com"
-    
-    #Generate Basic authorization key when it test mode
+
+    # Generate Basic authorization key when it test mode
     if environment_mode == "sandbox":
-      collections_apiuser = str(uuid.uuid4())
-      
-    #Create API user
+        collections_apiuser = str(uuid.uuid4())
+        disbursements_apiuser = str(uuid.uuid4())
+
+     # ================================================================================
+
+    # Create API user for collections
     url = ""+str(accurl)+"/v1_0/apiuser"
 
     payload = json.dumps({
-      "providerCallbackHost": "URL of host ie google.com"
+        "providerCallbackHost": "URL of host ie google.com"
     })
+
     headers = {
-      'X-Reference-Id': collections_apiuser,
-      'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': collections_subkey
+        'X-Reference-Id': collections_apiuser,
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': collections_subkey
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
-      
-    #Create API key
+
+    # Create API key for collections
     url = ""+str(accurl)+"/v1_0/apiuser/"+str(collections_apiuser)+"/apikey"
 
-    payload={}
+    payload = {}
     headers = {
-      'Ocp-Apim-Subscription-Key': collections_subkey
+        'Ocp-Apim-Subscription-Key': collections_subkey
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
+
     #print("The response is: \n"+str(response))
     response = response.json()
-    #Auto generate when in test mode
+
+    # Auto generate when in test mode
     if environment_mode == "sandbox":
-      api_key_collections = str(response["apiKey"])
+        api_key_collections = str(response["apiKey"])
 
-    #Create basic key for Collections
+    # Create basic key for Collections
     username, password = collections_apiuser, api_key_collections
-    basic_authorisation_collections = encoded_str = str(encode(username, password))
-    #print(basic_authorisation_collections)
 
-    #API User
+    basic_authorisation_collections = encoded_str = str(
+        encode(username, password))
+    # print(basic_authorisation_collections)
+
+    # API User
     #print("Api user:"+collections_apiuser+"\n")
     #print("Api Key:"+api_key_collections)
 
+    # ================================================================================
 
-    #Momo token generation
-    def  momotoken():
-          url = ""+str(PayClass.accurl)+"/collection/token/"
+    # Create API user for disbursements
+    url = ""+str(accurl)+"/v1_0/apiuser"
 
-          payload={}
-          headers = {
+    payload = json.dumps({
+        "providerCallbackHost": "URL of host ie google.com"
+    })
+
+    headers = {
+        'X-Reference-Id': disbursements_apiuser,
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': disbursements_subkey
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    # Create API key for disbursements
+    url = ""+str(accurl)+"/v1_0/apiuser/"+str(disbursements_apiuser)+"/apikey"
+
+    payload = {}
+    headers = {
+        'Ocp-Apim-Subscription-Key': disbursements_subkey
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    #print("The response is: \n"+str(response))
+    response = response.json()
+
+    # Auto generate when in test mode
+    if environment_mode == "sandbox":
+        api_key_disbursements = str(response["apiKey"])
+
+    # Create basic key for Collections
+    username, password = disbursements_apiuser, api_key_disbursements
+
+    basic_authorisation_disbursments = encoded_str = str(
+        encode(username, password))
+
+#     print(basic_authorisation_disbursments)
+
+    # API User
+    #print("Api user:"+collections_apiuser+"\n")
+#     print("Api Key:" + api_key_disbursements)
+
+    # ================================================================================
+
+    # Momo token generation
+
+    def momotoken():
+        url = ""+str(PayClass.accurl)+"/collection/token/"
+
+        payload = {}
+        headers = {
             'Ocp-Apim-Subscription-Key': PayClass.collections_subkey,
             'Authorization': str(PayClass.basic_authorisation_collections)
-          }
+        }
 
-          response = requests.request("POST", url, headers=headers, data=payload)
+        response = requests.request("POST", url, headers=headers, data=payload)
 
-          authorization_token = response.json()
-          return authorization_token
+        authorization_token = response.json()
 
-    #Momo disbursement token generation
-    def  momotokendisbursement():
-          url = ""+str(PayClass.accurl)+"/disbursement/token/"
+        return authorization_token
 
-          payload={}
-          headers = {
-            'Ocp-Apim-Subscription-Key': PayClass.disbursment_subkey,
-            'Authorization': 'Basic '+str(PayClass.basic_authorisation_disbursment)+''
-          }
+    # Momo disbursement token generation
+    def momotokendisbursement():
+        url = ""+str(PayClass.accurl)+"/disbursement/token/"
 
-          response = requests.request("POST", url, headers=headers, data=payload)
+        payload = {}
+        headers = {
+            'Ocp-Apim-Subscription-Key': PayClass.disbursements_subkey,
+            'Authorization': str(PayClass.basic_authorisation_disbursments)
+        }
 
-          authorization_token = response.json()
-          return authorization_token      
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        authorization_token = response.json()
+
+        return authorization_token
 
     def momopay(amount, currency, txt_ref, phone_number, payermessage):
-        #UUID V4 generator
+        # UUID V4 generator
         uuidgen = str(uuid.uuid4())
         url = ""+str(PayClass.accurl)+"/collection/v1_0/requesttopay"
 
         payload = json.dumps({
-          "amount": amount,
-          "currency": currency,
-          "externalId": txt_ref,
-          "payer": {
-            "partyIdType": "MSISDN",
-            "partyId": phone_number
-          },
-          "payerMessage": payermessage,
-          "payeeNote": payermessage
+            "amount": amount,
+            "currency": currency,
+            "externalId": txt_ref,
+            "payer": {
+                "partyIdType": "MSISDN",
+                "partyId": phone_number
+            },
+            "payerMessage": payermessage,
+            "payeeNote": payermessage
         })
         headers = {
-  'X-Reference-Id': uuidgen,
-  'X-Target-Environment': PayClass.environment_mode,
-  'Ocp-Apim-Subscription-Key': PayClass.collections_subkey,
-  'Content-Type': 'application/json',
-  'Authorization': "Bearer "+str(PayClass.momotoken()["access_token"])
-}
+            'X-Reference-Id': uuidgen,
+            'X-Target-Environment': PayClass.environment_mode,
+            'Ocp-Apim-Subscription-Key': PayClass.collections_subkey,
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer "+str(PayClass.momotoken()["access_token"])
+        }
 
         response = requests.request("POST", url, headers=headers, data=payload)
 
-        context = {"response":response.status_code, "ref":uuidgen}
-       
+        context = {"response": response.status_code, "ref": uuidgen}
+
         return context
 
     def verifymomo(txn):
-       url = ""+str(PayClass.accurl)+"/collection/v1_0/requesttopay/"+str(txn)+""
+        url = ""+str(PayClass.accurl) + \
+            "/collection/v1_0/requesttopay/"+str(txn)+""
 
-       payload={}
-       headers = {
-         'Ocp-Apim-Subscription-Key': PayClass.collections_subkey,
-         'Authorization':  "Bearer "+str(PayClass.momotoken()["access_token"]),
-           'X-Target-Environment': PayClass.environment_mode,
-       }
+        payload = {}
+        headers = {
+            'Ocp-Apim-Subscription-Key': PayClass.collections_subkey,
+            'Authorization':  "Bearer "+str(PayClass.momotoken()["access_token"]),
+            'X-Target-Environment': PayClass.environment_mode,
+        }
 
-       response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-       json_respon = response.json()
+        json_respon = response.json()
 
-       return json_respon
+        return json_respon
 
-    #Check momo collections balance
+    # Check momo collections balance
     def momobalance():
-       url = ""+str(PayClass.accurl)+"/collection/v1_0/account/balance"
+        url = ""+str(PayClass.accurl)+"/collection/v1_0/account/balance"
 
-       payload={}
-       headers = {
-         'Ocp-Apim-Subscription-Key': PayClass.collections_subkey,
-         'Authorization':  "Bearer "+str(PayClass.momotoken()["access_token"]),
-           'X-Target-Environment': PayClass.environment_mode,
-       }
+        payload = {}
+        headers = {
+            'Ocp-Apim-Subscription-Key': PayClass.collections_subkey,
+            'Authorization':  "Bearer "+str(PayClass.momotoken()["access_token"]),
+            'X-Target-Environment': PayClass.environment_mode,
+        }
 
-       response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-       json_respon = response.json()
+        json_respon = response.json()
 
-       return json_respon 
-    
-    #Check Disubursement balance
+        return json_respon
+
+    # Check Disubursement balance
     def momobalancedisbursement():
-       url = ""+str(PayClass.accurl)+"/disbursement/v1_0/account/balance"
+        url = ""+str(PayClass.accurl)+"/disbursement/v1_0/account/balance"
 
-       payload={}
-       headers = {
-         'Ocp-Apim-Subscription-Key': PayClass.momobalancedisbursement,
-         'Authorization':  "Bearer "+str(PayClass.momotokendisbursement()["access_token"]),
-           'X-Target-Environment': PayClass.environment_mode,
-       }
+        payload = {}
+        headers = {
+            'Ocp-Apim-Subscription-Key': PayClass.momobalancedisbursement,
+            'Authorization':  "Bearer "+str(PayClass.momotokendisbursement()["access_token"]),
+            'X-Target-Environment': PayClass.environment_mode,
+        }
 
-       response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-       json_respon = response.json()
+        json_respon = response.json()
 
-       return json_respon   
+        return json_respon
 
+   # Withdraw money Disbursement
 
-  #Withdraw money Disbursement
     def withdrawmtnmomo(amount, currency, txt_ref, phone_number, payermessage):
-        #UUID V4 generator
+        # UUID V4 generator
         uuidgen = str(uuid.uuid4())
         url = ""+str(PayClass.accurl)+"/disbursement/v1_0/transfer"
 
@@ -194,54 +254,49 @@ class PayClass():
             "currency": currency,
             "externalId": txt_ref,
             "payee": {
-              "partyIdType": "MSISDN",
-              "partyId": phone_number
+                "partyIdType": "MSISDN",
+                "partyId": phone_number
             },
             "payerMessage": payermessage,
             "payeeNote": payermessage
         })
 
-  
         headers = {
-  'X-Reference-Id': uuidgen,
-  'X-Target-Environment': PayClass.environment_mode,
-  'Ocp-Apim-Subscription-Key': PayClass.disbursment_subkey,
-  'Content-Type': 'application/json',
-  'Authorization': "Bearer "+str(PayClass.momotokendisbursement()["access_token"])
-}
+            'X-Reference-Id': uuidgen,
+            'X-Target-Environment': PayClass.environment_mode,
+            'Ocp-Apim-Subscription-Key': PayClass.disbursements_subkey,
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer "+str(PayClass.momotokendisbursement()["access_token"])
+        }
 
         response = requests.request("POST", url, headers=headers, data=payload)
 
-        context = {"response":response.status_code, "ref":uuidgen}
-     
-        return context        
+        context = {"response": response.status_code, "ref": uuidgen}
 
-#Check transfer status disbursment
+        return context
+
+# Check transfer status disbursment
     def checkwithdrawstatus(txt_ref):
-        #UUID V4 generator
+        # UUID V4 generator
         uuidgen = str(uuid.uuid4())
-        url = ""+str(PayClass.accurl)+"/disbursement/v1_0/transfer/"+str(txt_ref)+""
+        url = ""+str(PayClass.accurl) + \
+            "/disbursement/v1_0/transfer/"+str(txt_ref)+""
 
         payload = json.dumps({
-           
+
         })
 
-  
         headers = {
-  'X-Reference-Id': uuidgen,
-  'X-Target-Environment': PayClass.environment_mode,
-  'Ocp-Apim-Subscription-Key': PayClass.disbursment_subkey,
-  'Content-Type': 'application/json',
-  'Authorization': "Bearer "+str(PayClass.momotokendisbursement()["access_token"])
-}
+            'X-Reference-Id': uuidgen,
+            'X-Target-Environment': PayClass.environment_mode,
+            'Ocp-Apim-Subscription-Key': PayClass.disbursements_subkey,
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer "+str(PayClass.momotokendisbursement()["access_token"])
+        }
 
         response = requests.request("GET", url, headers=headers, data=payload)
         returneddata = response.json()
-        context = {"response":response.status_code, "ref":txt_ref, "data":returneddata}
-     
+        context = {"response": response.status_code,
+                   "ref": txt_ref, "data": returneddata}
+
         return context
-
-
-
-
-
